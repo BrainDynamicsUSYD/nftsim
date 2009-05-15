b0648c36932ddbc86eef0b4b68a5f83482823c8e
peter <peter@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2009-05-15 00:03:00 +0000

    Preliminary implementation of modsigma

---
+++ EEGcode/trunk/modsigma.cpp
@@ -0,0 +1,60 @@
+/***************************************************************************
+                          modsigma.cpp  -  variance dynamics model for modulating
+                                           sigma
+                             -------------------
+   copyright            : (C) 2009 by Peter Drysdale
+    email                : peter@physics.usyd.edu.au
+ ***************************************************************************/
+
+#include <math.h>
+#include<string>
+using std::string;
+#include<sstream>
+using std::stringstream;
+#include "modsigma.h"
+
+Modsigma::Modsigma(Istrm& inputf, int popindex){
+  double temp;
+  inputf.validate("SigmaTheta",58);
+  inputf >> temp;
+  sigmatheta2=temp*temp;
+  inputf.validate("Initial SigmaV",58);
+  inputf >> sigmaVpast;
+  inputf.validate("Av. length",58);
+  inputf >> avlength;
+// 
+  initoutput(inputf,popindex);
+}
+
+Modsigma::~Modsigma(){
+}
+
+void Modsigma::dump(ofstream& dumpf){
+  dumpf << " SigmaTheta: " << sqrt(sigmatheta2);
+  dumpf << " Initial SigmaV: " << sigmaVpast;
+  dumpf << " Av. length: " << avlength;
+}
+
+double Modsigma::get(double timestep, double* Vab){
+  double sigma;
+  double sigmaV;
+  sigmaV=exp(-timestep/avlength)*sigmaVpast+Vab[1];
+  sigma= sqrt(sigmatheta2+sigmaV*sigmaV);
+  output();
+  return (sigma);
+}
+
+void Modsigma::initoutput(Istrm& inputf, int popindex){
+  popindex++; // (popindex+1) is to ensure numbering from one not zero in output
+  stringstream ss(stringstream::in | stringstream::out);
+  ss << "eegcode.sigmaout." << popindex;
+  sigmaoutf.open(ss.str().c_str(),ios::out);
+  if( !sigmaoutf ){
+    cerr << "Unable to open 'eegcode.sigmaout." << popindex << "' for output \n";
+    exit(EXIT_FAILURE);
+  }
+  sigmaoutf << "Robinson varinace dynamics model output data" << endl;
+}
+
+void Modsigma::output(){
+}
---
+++ EEGcode/trunk/modsigma.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+                          modsigma.h  - variance dynamics sigma modulation 
+                             -------------------
+    copyright            : (C) 2009 by Peter Drysdale
+    email                : peter@physics.usyd.edu.au
+ ***************************************************************************/
+
+#ifndef MODSIGMA_H
+#define MODSIGMA_H
+
+#include<fstream>
+using std::ofstream;
+#include<iostream>
+using std::cerr;
+using std::endl;
+#include<iomanip>
+using std::setprecision;
+
+#include"istrm.h"
+
+class Modsigma {
+public: 
+  Modsigma(Istrm& inputf, int popindex);
+  ~Modsigma();
+  void dump(ofstream& dumpf); // dump ithreshold params for restart
+  double get(double timestep, double * Vab); // return theta value for modulated threshold
+
+private:
+  void initoutput(Istrm& inputf, int popindex); // initialize output routine 
+  void output(); // output variance variables as needed
+  long nodes;
+  double sigmatheta2; // Parameter sigma_{\theta}^2
+  double sigmaVpast; // Parameter sigma_{V} one step in the past
+  double avlength; // Parameter characteristic length scale of exponentially
+                   // weighted averaging
+  ofstream sigmaoutf; // Output stream "eegcode.sigmaout"
+};
+
+#endif
