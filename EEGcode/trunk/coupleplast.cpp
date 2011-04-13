c11c073bde8ea00946fbb5b5234006835dc2bf53
ffung <ffung@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2011-04-13 06:05:35 +0000

    Updates coupleplast

--- EEGcode/trunk/coupleplast.cpp
+++ EEGcode/trunk/coupleplast.cpp
@@ -7,6 +7,7 @@
  ***************************************************************************/
 
 #include <math.h>
+#include <complex>
 #include<string>
 using std::string;
 #include<sstream>
@@ -14,29 +15,90 @@ using std::stringstream;
 #include<iomanip>
 using std::setprecision;
 #include "coupleplast.h"
+#include "rect.h"
+#include <iostream>
 using std::endl;
+using namespace std;
 
 Coupleplast::Coupleplast(long numnodes, double deltat)
-  :t(0),nodes(numnodes),timestep(deltat){
+  :t(0),nodes(numnodes),timestep(deltat), i0(0.,1.), c1(1.,0.){
 }
 
 Coupleplast::~Coupleplast(){
+  //delete alphaobj;
+  //delete betaobj;
+  if(synapoutf.is_open()) synapoutf.close();
 }
 
 void Coupleplast::init(Istrm& inputf, int coupleid){
-// Initialise any variables
+  //alphaobj = new Parameter("Alpha",inputf);
+  //betaobj = new Parameter("Beta",inputf);
+  Parameter* nuobj = new Parameter("Nu",inputf);
+  nu = nuobj->get();
+  delete nuobj;
+  coupleid++;
+  stringstream ss(stringstream::in|stringstream::out);
+  ss<<"neurofield.synaptout."<<coupleid;
+  synapoutf.open(ss.str().c_str(),std::ios::out);
+  if(!synapoutf){
+    std::cerr<<"Unable to open 'neurofield.synaptout."<<coupleid<<"' for output.\n";
+    exit(EXIT_FAILURE);
+  }
+  //double G[2] = nuobj->get()*4200;
+  double G_e = 0;//G[0];
+  double G_i = 0;//G[1];
+  double alpha = 75; double beta = 285;
+  static complex<double> L = c1/(c1-i0*w/alpha)/(c1-i0*w/beta);
+  double Aplus = 1; double Aminus = -0.4; double tauplus = 0.02; double tauminus = 0.04;
+  static complex<double> H = Aplus*tauplus/(c1-i0*w*tauplus)
+        + Aminus*tauminus/(c1+i0*w*tauminus);
+  double gamma_e = 116; double gamma_i = 1500;
+  static complex<double> Gamma_e = c1/pow(c1-i0*w/gamma_e,2);
+  static complex<double> Gamma_i = c1/pow(c1-i0*w/gamma_i,2);
+  static double constant = pow(abs(L),2)*real(conj(H)*conj(Gamma_e));
 }
 
 void Coupleplast::dump(ofstream& dumpf){
-// Output any variables required to do a restart
+  dumpf << "Nu:" << nu << " ";
+  //alphaobj->dump(dumpf);
+  //betaobj->dump(dumpf);
 }
 
 void Coupleplast::output(){
-// Output variables for data analysis
+  synapoutf<<setprecision(14)<<nu<<endl;
+}
+
+double Coupleplast::get(void) const
+{
+  return nu;
 }
 
 //
 // Sum the coupling terms transforming Phi_{ab} to P_{ab}
 //
 void Coupleplast::updatePa(double *Pa, double *Etaa,Qhistorylist& qhistorylist,ConnectMat& connectmat){
+  long n=nodes;
+  rect Int;
+  double x0 = 0.; double x1 = 5000.; double dx = 1.;
+  double integrand[int(x1-x0)/int(dx)];
+  for( int i = 0; i<(x1-x0)/dx; i++ ){
+    integrand[i] = f(x0+i*dx);
+    //std::cout<<x0+i*dx<<" "<<integrand[i]<<std::endl;
+    std::cout<<c1;
+  }
+  exit(0);
+  nu += Int.Int( 0., 5000., 1., const_cast<const double* const>(integrand) );
+  double result = 0;
+  for( int i=0; i<=(x1-x0)/dx; i++ )
+    result += Integrand[i];
+    //result += f( x0+i*dx );
+  return result*dx;
+  for(int i=0; i<n; i++)
+    Pa[i]=nu*Etaa[i];
+}
+
+double Coupleplast::f( double w )
+{
+  complex<double> detA = c1 -L*Gamma_e*G_e -L*Gamma_i*G_i;
+  return pow(abs(c1/detA),2)*constant;
 }
--- EEGcode/trunk/coupleplast.h
+++ EEGcode/trunk/coupleplast.h
@@ -11,11 +11,13 @@
 #include<fstream>
 using std::ofstream;
 #include<iostream>
+#include<complex>
 #include"istrm.h"
 #include"couple.h"
 #include"timeseries.h"
 #include"qhistorylist.h"
 #include"connectmat.h"
+#include"parameter.h"
 
 class Coupleplast: public Couple {
 public: 
@@ -25,12 +27,20 @@ public:
   void dump(ofstream& dumpf); // output values for restart
   void output(); // output variables as needed
   void updatePa(double *Pa,double *Etaa,Qhistorylist& qhistorylist,ConnectMat& connectmat);
+  double get(void) const; // return nu of this population
 
 private:
+  double f( double w ); // returns the integrand
   Coupleplast(Coupleplast& ); // no copy constructor
   double t; //current time
+  double nu;
+  Parameter* alphaobj;
+  Parameter* betaobj;
   const long nodes;
   double timestep;
+  ofstream synapoutf;
+  std::complex<double> i0; // purely imaginary number i
+  std::complex<double> c1; // real number 1
 };
 
 #endif
