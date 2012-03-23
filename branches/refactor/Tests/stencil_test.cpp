dd8ffc47a715a00ad8ec9b3635188e7dc67cec48
romesh <romesh@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2012-03-23 06:46:05 +0000

    Updated unit tests

--- branches/refactor/Tests/stencil_test.cpp
+++ branches/refactor/Tests/stencil_test.cpp
@@ -5,40 +5,42 @@
 
 using namespace std;
 
+void ce(Stencil* s){
+    cout << s->c << " " << s->e << endl;
+    return;
+}
+
 int main(){
-    vector<int> x;
-    Stencil* old[2];
+    vector<double> x;
+    vector<double> y;
+    Stencil* s;
     
-    for(int i = 1; i<=25; i++)
+    for(int i = 1; i<=25; i++){
         x.push_back(i);
+        y.push_back(i*2);
+    }
     
-    old[1] = new Stencil(25,5);
-    old[2] = new Stencil(25,5);
+    s = new Stencil(25,5);
+    s -> assign(&x);
     
-    old[1]->assign(&x);
-    old[2]->assign(&x);
-    
-    cout << "Test 1: should see '1 2', '5 1' and '1 2'" << endl;
-    cout << old[1]->c << " " << old[1]->e << endl;
+    cout << "Test 1: should see '1 2', '5 1' and '2 3'" << endl;
+    ce(s);
     for(int i = 0; i < 4; i++)
-        old[1]++;
-    cout << old[1]->c << " " << old[1]->e << endl;
-    for(int i = 0; i < 20; i++)
-        old[1]++;
-    cout << old[1]->c << " " << old[1]->e << endl;
+        s++;
+    ce(s);
+    for(int i = 0; i <= 20; i++)
+        s++;
+    ce(s);
 
-    cout << "Test 2: should see '1 2' followed by '1 2'" << endl;
-    cout << old[2]->c << " " << old[2] -> e << endl;
-    for(int i = 0; i <= 24; i++)
-        old[2]++;
-    *old[1] = *old[2];
-    cout << old[1]->c << " " << old[1]->e << endl;
-
+    cout << "Test 2: should see '2 4'" << endl;
+    s->assign(&y);
+    ce(s);
+    
     cout << "Test 3: should see 2925" << endl;
-    old[2] -> assign(&x);
+    s->assign(&x);
     int total = 0;
-    for(int i = 0; i <= 24; i++)
-        total += old[2]->ne + old[2]->n + old[2]->nw + old[2]->e + old[2]->c + old[2]->w + old[2]->se + old[2]->s + old[2]->sw;
+    for(int i = 0; i <= 24; i++,s++)
+        total += s->ne + s->n + s->nw + s->e + s->c + s->w + s->se + s->s + s->sw;
     cout << total << endl;
      
     return 0;
