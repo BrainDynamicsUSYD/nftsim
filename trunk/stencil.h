578876197a37763d0ff2c8e89328da1c39f34f08
ffung <ffung@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2012-04-12 06:32:23 +0000

    Reimplemented stencil algorithm using original Drysdale algorithm.

--- trunk/stencil.h
+++ trunk/stencil.h
@@ -1,39 +1,37 @@
 #ifndef STENCIL_H
 #define STENCIL_H
 
+#include<cstdlib>
+#include<string>
+using std::string;
 #include<vector>
 using std::vector;
 
 class Stencil
 {
-  Stencil(); // no default constructor
+  Stencil();
   Stencil& operator=(const Stencil&);
   Stencil(const Stencil&);
-
-  double bath;
 protected:
   int nodes;
   int longside;
-  vector<double>* m;
+  string boundary;
+  vector<double> m;
 
-  // pointers to the Moore grid
-  int _nw; int _n; int _ne;
-  int  _w; int _c; int  _e;
-  int _sw; int _s; int _se;
+  mutable int ptr;
 public:
-  Stencil( int nodes, int longside, double bath=0 );
+  Stencil( int nodes, int longside, const string& boundary );
   virtual ~Stencil(void);
 
-  void assign( vector<double>* field );
+  const vector<double>& operator= ( const vector<double>& field );
+  virtual void operator++ (int i) const; // increment Moore grid
+  void set( int node ) const; // point to node
+  int get(void) const; // get ptr
 
-  virtual void operator++(int i); // increment Moore grid
-  virtual void set( int node ); // point to node
-  int get(void) const; // get current node index
-
-  // get Moore grid
-  virtual double nw()const; virtual double n()const; virtual double ne()const;
-  virtual double  w()const; virtual double c()const; virtual double  e()const;
-  virtual double sw()const; virtual double s()const; virtual double se()const;
+  // get Moore grid values
+  double nw() const; double n() const; double ne() const;
+  double  w() const; double c() const; double  e() const;
+  double sw() const; double s() const; double se() const;
 };
 
 #endif
