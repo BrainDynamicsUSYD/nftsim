d277a21b45bef6496aa0cacf23681c212d3af87b
xzhao <xzhao@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2012-04-30 06:19:41 +0000

    Added some actual code to burst.cpp and burst.h

--- trunk/burst.cpp
+++ trunk/burst.cpp
@@ -1,10 +1,24 @@
 #include"burst.h"
-// look up qresponse.cpp for example
 
 void BurstResponse::init( Configf& inputf )
 {
- // read config file- see qresponse.init()
+  if( configf.optional("Theta",theta) )) {
+	  configf.param(("Sigma",sigma);
+	  configf.param("Qmax",Q_max);
+  }
+  configf>>dendrites;
+}// read config file- see qresponse.init()
  // initialize any other private variables (i.e. current)
+Ia
+Ib
+Ic
+Taux
+Tauh
+Ax
+Mu
+Xtilda
+Htilda
+
 }
 
 void BurstResponse::restart( Restartf& restartf )
@@ -43,12 +57,14 @@ void BurstResponse::step(void)
 
 void BurstResponse::fire( vector<double>& Q ) const
 {
- // map voltage into firing rate
+  if(theta)
+	  for( int i=0; i<nodes; i++ )
+	    Q[i]=Q_max/(1.0F+ exp( -(v[i]-theta)/sigma ) ) // map voltage into firing rate
 }
 
 const vector<double>& BurstResponse::V(void) const
 {
-  // leave empty if you like, it just returns v
+  return v; // leave empty if you like, it just returns v
 }
 
 vector<Output*> BurstResponse::output(void) const
@@ -60,4 +76,35 @@ vector<Output*> BurstResponse::output(void) const
   return vector<Output*>(1,temp);
 }
 
-// Write an rk4 function here
+rk4(double t,double* y, float timestep, double* V, double Qmax, double sigma) {
+	// Write an rk4 function here
+	double yt[2];
+	double dyt[2];
+	double dym[2];
+	double h=timestep;
+	double h2=timestep/2;
+	double h6=timestep/6;
+
+	rkderivs(t,y,dydt,V,Qmax,sigma);
+	for(int i=0; i<2; i++) yt[i]=y[i]+h2*dydt[i];
+	rkderivs(t+h2,yt,dyt,V,Qmax,sigma);
+	for(int i=0; i<2; i++) yt[i]=y[i]+h2*dyt[i];
+	rkderivs(t+h2,yt,dym,V,Qmax,sigma);
+	for(int i=0; i<2; i++) {yt[i]=y[i]+h*dym[i]; dym[i]+=dyt[i];}
+	rkderivs(t+h,yt,dyt,V,Qmax,sigma);
+	for(int i=0; i<2;; i++) y[i]=y[i]+h6*(dydt[i]+dyt[i]+2*dym[i]);
+}
+
+void rkderivs(double steptime, double* y, double* dydt, double* V, double Qmax,
+		double sigma){
+	//function returns the derivatives for modulated threshold eqns
+	//y[0] is Htilda , y[1] is Xtilda
+
+	double thetatemp=(Ic-3*Ib*y[1]+(Ib-Ia)*y[0])/mu;
+	double Qfiring=Qmax/(1+exp(-(V[1]-thetatemp)/sigma));
+	double xinfinity;
+	if((Qfiring*Ax)>0) {xinfinity =Ax*Qfiring;}
+	else {xinfinity=0;}
+	dydt[0]=(3*y[1]-y[0])/tauh; // Htilde DE
+	dydt[1]=(xinfinity-y[1])/taux; // Xtilde DE
+}
--- trunk/burst.h
+++ trunk/burst.h
@@ -4,21 +4,23 @@
 #include"qresponse.h"
 #include"output.h"
 
-class BurstResponse : public QResponse
+class BurstResponse : public QResponse //derived class; constructor initializer
 {
-  BurstResponse(BurstResponse& ); // no copy constructor
-  BurstResponse(void);      // no copy constructor
+  BurstResponse(BurstResponse& ); // no copy constructor; no explicit var name
+  BurstResponse(void);      // no copy constructor; void no return value
 protected:
   void init( Configf& inputf );
   void restart( Restartf& restartf );
-  void dump( Dumpf& dumpf ) const;
+  void dump( Dumpf& dumpf ) const; //const member fnctn does not change obj
 public: 
   BurstResponse( int nodes, double deltat, int index );
-  virtual ~BurstResponse(void);
+  virtual ~BurstResponse(void); //destructor mem fnctn 
+								//virtual= needs redef in derived classes
   void step(void);
 
-  void fire( vector<double>& Q ) const;
+  void fire( vector<double>& Q ) const; //const refers to object called on
   const vector<double>& V(void) const;
+  vector<Output*> output(void) const; //vector of Output ptrs filled by
 };
 
 #endif
