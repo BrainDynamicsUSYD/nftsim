104a4d077e7b42899efb3b121c4855b625d8437f
romesh <romesh@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2012-07-06 03:57:20 +0000

    Made some changes to the NF spatial spectrum helpers

--- Helper_scripts/nf_spatial_filter.m
+++ Helper_scripts/nf_spatial_filter.m
@@ -1,18 +1,86 @@
-function [f,Pnew,Pold,filtered_nf] = nf_spatial_filter(nf,traces,p)
+function [f,P,Pold] = nf_spatial_filter(nf,traces,p)
     % [f,P,P_old,filtered_nf] = nf_spatial_filter(nf,traces)
     % Given a grid of voltages, applies a spatial filter   
     % Returns the power spectrum calculated from the centre node of the trace
     % And optionally the spatially smoothed matrix
     % Method from http://www.mathworks.com/matlabcentral/answers/13896-fftshift-of-an-image
-    % First, work out the sampling rate in pixels per metre
-    data = nf_grid(nf,traces);
     
-    % Construct the spatial filter
-    v = data(:,:,1);
+    % First, work out the sampling rate in pixels per metre
+    if strcmp(class(nf),'struct')
+        data = nf_grid(nf,traces);
+        fs = 1/nf.deltat;
+    else
+        data = nf;
+        fs = traces;
+    end
+        
+    n_windows = 8; % like pwelch, 8 windows, 50% overlap
+    frac_overlap = 0.5;
+    window_vectors = get_window_vectors(size(data,3),n_windows,frac_overlap); 
+
+    % Calculate the Fourier f and k values
     Lx = 0.5; % linear cortex dimension (m)
     Ly = 0.5;
+    [f,Kx,Ky] = calculate_fft_components(data(:,:,window_vectors{1}),fs,Lx,Ly);
+
     k0 = 10; % spatial filter constant (m^-1)
-        
+    k2 = Kx.^2+Ky.^2; % Matrix of k-squared values
+    k_filter = exp(-k2./(k0^2));
+    
+    kmax = 2; % k values to include
+    [center_x,center_y] = find(k2 == 0); % Get the centre entry
+    [a,b] = meshgrid(1:size(k2,1),1:size(k2,2));
+    k_mask = abs(a-center_x) <= kmax & abs(b-center_y) <= kmax;
+    [fold,~,Pold] = rfft(squeeze(data(center_x,center_y,:)),fs);
+    
+    P = zeros(size(f));
+    for j = 1:length(window_vectors)
+        P = P + get_3d_spectrum(data(:,:,window_vectors{j}),k_mask,k_filter,Lx,fs);
+    end
+    P = P/length(window_vectors);
+    P = P*100;
+    %return
+    
+    %close all
+    figure
+    loglog(f,P,fold,Pold)
+    %loglog(f,P);
+    
+    if nargin == 3
+        [f2,P2] = analytic_spectrum(p,1);
+        hold on
+        loglog(f2,P2,'r');
+        legend('Filtered','Original NF','Analytic');
+    end
+
+function P = get_3d_spectrum(data,k_mask,k_filter,Lx,fs)
+    data = data - mean(data(:));
+    win(1,1,:) = hamming(size(data,3));
+    data = bsxfun(@times,data,win);
+    output = fftshift(fftn(data));
+    output = output./numel(data);
+    
+    % Convert to power, and filter
+    output = abs(output).^2;
+    % Convert to energy
+    
+    output = output * Lx * Lx * size(data,3) / fs;
+
+    %output = output*0.001;
+    output = bsxfun(@times,output,k_mask);
+    output = bsxfun(@times,output,k_filter);
+
+    %output = output * Lx * Lx * size(data,3) / fs;
+    output = output / Lx / Lx; % Account for integration by summation. 
+    P = squeeze(sum(sum(output,1),2));
+    P = ifftshift(P);
+    P = 2*P(1:size(data,3)/2+1);
+    % Todo: exclude zero and nyquist frequency parts from this multiplication
+    
+function [f,Kx,Ky] = calculate_fft_components(v,fs,Lx,Ly)
+    % Given the 3D matrix of phi, the temporal sampling rate, and the grid dimensions
+    % Return the frequencies and wavenumbers that are provided by the FFT
+    
     Fs_x = size(v,1)/Lx; % Pixels per *metre* OK
     Fs_y = size(v,2)/Ly;
     
@@ -34,45 +102,23 @@ function [f,Pnew,Pold,filtered_nf] = nf_spatial_filter(nf,traces,p)
     Kx = Fx*2*pi; % wavenumber
     Ky = Fy*2*pi;
     [Kx,Ky] = meshgrid(Kx,Ky);
-    k2_matrix = Kx.^2+Ky.^2;
-    
-    kmax = 2; % k values to include
-    m_rows = -kmax:kmax; % The code will panic and fail if there are not an odd
-    n_cols = -kmax:kmax; % number of k values. This ensures the convolution is properly symmetric
-    [center_x,center_y] = find(k2_matrix == 0) % Get the centre entry
-    
-    decimation_factor = 1;%1/200/nf.deltat;
-    decimated_rate = 1/nf.deltat/decimation_factor;
-    vold = decimate(detrend(data(center_x,center_y,:)),decimation_factor);
-    [f,Pold] = pwelch_spectrum(vold,decimated_rate);
-    Pnew = zeros(size(Pold));
-    
-    % FFT to provide V(k,t) - I *think* this is correct? Or may be P(k,t)...
-    parfor j = 1:size(data,3)
-        output(:,:,j) = fftshift(fft2(data(:,:,j)));
-    end
-    output = output./2500;
+    f = fs/2*linspace(0,1,size(v,3)/2+1)';
 
-    for m = 0
-        for n = 0
-            v_temp = decimate(detrend(output(center_x+m,center_y+n,:)),decimation_factor);
-            [~,P] = pwelch_spectrum(v_temp,decimated_rate);
-            
-            k2 = (2*pi*m/Lx)^2 + (2*pi*n/Ly)^2;
-            Pnew = Pnew + P.*exp(-k2/k0^2);
-        end    
-    end    
-    
-    filtered_nf = nf;
-    filtered_nf.data{1} = reshape(shiftdim(output,2),[],2500);
-    
-    close all
-    figure
-    loglog(f,Pnew,f,Pold)
-    
-    if nargin == 3
-        [f2,P2] = analytic_spectrum(p,1);
-        hold on
-        loglog(f2,P2,'r');
-        legend('Filtered','Original NF','Analytic');
+function window_vectors = get_window_vectors(l_sample,n_windows,frac_overlap)
+    % Return a cell array with indices for each window
+    % Sample is calculated from n windows overlapping by frac_overlap
+    % pwelch uses n_windows = 8, frac_overlap = 0.5 by default
+    window_length = l_sample/((1+(n_windows-1)*(1-frac_overlap)));
+    %window_length = floor(window_length);
+    window_start = 1:window_length*(1-frac_overlap):window_length*(1-frac_overlap)*n_windows;
+    window_start = floor(window_start);
+    window_stop = window_start+floor(window_length)-1;
+    if window_stop > l_sample
+        error('Error calculating indices!');
     end
+    window_vectors = cell(1,n_windows);
+    for j = 1:n_windows
+        window_vectors{j} = window_start(j):window_stop(j);
+    end
+    
+    
--- Helper_scripts/nf_spectrum.m
+++ Helper_scripts/nf_spectrum.m
@@ -1,7 +1,25 @@
 function [f,P] = nf_spectrum( nf, data )
     % return the frequency and frequency spectrum of given data
     % nf is parsed for information on sampling time
-    decimation_factor = 1;
-    v = decimate( detrend(data), decimation_factor );
-    [f,P] = pwelch_spectrum( v, 1/nf.deltat/decimation_factor );
+    % data can be a string specifying a trace, or a vector
+    
+    if strcmp(class(data),'char')
+        data = nf_extract(nf,data);
+    end
+    
+    decimation_factor = (1/nf.deltat)/200; % Downsample to 200Hz
+    if decimation_factor ~= round(decimation_factor) % Unless it is not integer
+        decimation_factor = 1;
+    end
+    
+    v = decimate(data, decimation_factor );
+    [f,~,P] = rfft(v.*hamming(length(v)), 1/nf.deltat/decimation_factor );
+    
+    figure
+    loglog(f,P);
+    set(gca,'XLim',[1 45]);
+    xlabel('Frequency (Hz)');
+    ylabel('P(f) Spectral Power Density (arbitrary normalization)');
+    title(sprintf('Power spectrum (at %iHz sampling)',1/nf.deltat/decimation_factor));
+    
 end
