ae2cf00c0d7facac9603892d9af2c003bcf8fbdd
ffung <ffung@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2012-07-17 05:02:46 +0000

    CaRK4 is now renamed CaDP, so that CaDP now uses RK4 algorithm. The original CaDP implementation is deleted.

--- trunk/cadp.h
+++ trunk/cadp.h
@@ -1,35 +1,42 @@
-#ifndef CADP_H
-#define CADP_H
+#ifndef CARK4_H
+#define CARK4_H
 
 #include"couple.h"
+#include"de.h"
 
 class CaDP : public virtual Couple
 {
   CaDP();
   CaDP(CaDP&);
 
-  inline double sig( double x, double beta ) const;
-  inline double x(double Ca) const; // potentiation rate
-  inline double y(double Ca) const; // depression rate
+  struct CaDE : public RK4
+  {
+    double B; // 1/stanard deviation of glutamate binding
+    double glu_0; // glutamte dose-response threshold
 
-  vector<double> binding; // glutamate binding
+    double max; // maximum synaptic strength
+    double th;  // threshold time-scale of plasticity
+    double ltd; // time-scale of depression
+    double ltp; // time-scale of potentiation
 
-  double B; // 1/stanard deviation of glutamate binding
-  double glu_0; // glutamte dose-response threshold
+    double dth; // calcium threshold to depression
+    double pth; // calcium threshold to potentiation
 
-  double max; // maximum synaptic strength
-  double th; // threshold time-scale of plasticity
-  double ltd; // time-scale of depression
-  double ltp; // time-scale of potentiation
+    double tCa; // time-scale of calcium influx/cascade
 
-  double tCa; // time-scale of calcium influx/cascade
+    double gnmda; // gain for NMDA receptor
+
+	double pos; // sign of nu
+
+    CaDE( int nodes, double deltat ) : RK4(4,nodes,deltat) {}
+    virtual ~CaDE(void) {}
+    void rhs( const vector<double>& y, vector<double>& dydt );
+    inline double sig( double x, double beta ) const;
+    inline double po(double Ca) const; // potentiation rate
+    inline double de(double Ca) const; // depression rate
+  };
 protected:
-  vector<double> Ca;
-  vector<double> nhu; // transient coupling strength
-  double dth; // calcium threshold to depression
-  double pth; // calcium threshold to potentiation
-  vector<double> g; // gain for the NMDA receptor
-
+  CaDE rk4;
   virtual void init( Configf& configf );
   virtual void restart( Restartf& restartf );
   virtual void dump( Dumpf& dumpf ) const;
@@ -38,6 +45,7 @@ public:
           const Propag& prepropag, const Population& postpop );
   virtual ~CaDP(void);
   virtual void step(void);
+  virtual const vector<double>& nu(void) const;
   virtual vector<Output*> output(void) const;
 };
 
