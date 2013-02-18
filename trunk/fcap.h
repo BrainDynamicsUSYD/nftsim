b783791f5bf6586ce9e0c9578364ca6943be355d
ffung <ffung@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2013-02-18 03:54:56 +0000

    Implemented integer order DE for fCaP::x & y.

--- trunk/fcap.h
+++ trunk/fcap.h
@@ -24,27 +24,52 @@ protected:
 
   struct fCaDE : public CaDE
   {
-    double alpha, beta;
+    /*double alpha, beta;
     double lambda_x, mu_x;
     double lambda_y, mu_y;
     vector<FractionalIntegral*> x1;
     vector<FractionalIntegral*> x2;
     vector<FractionalIntegral*> y1;
-    vector<FractionalIntegral*> y2;
+    vector<FractionalIntegral*> y2;*/
     virtual void init( Configf& configf );
     void rhs( const vector<double>& y, vector<double>& dydt );
     fCaDE( int nodes, double deltat )
-        : CaDE(nodes,deltat), x1(nodes), x2(nodes), y1(nodes), y2(nodes) {}
+        : CaDE(nodes,deltat), /*, x1(nodes), x2(nodes), y1(nodes), y2(nodes)*/
+          xresponse(nodes,deltat,variables[4]),
+          yresponse(nodes,deltat,variables[5]) {}
     virtual ~fCaDE(void) {
-      for( int i=0; i<nodes; i++ ) {
+      /*for( int i=0; i<nodes; i++ ) {
         delete x1[i];
         delete x2[i];
         delete y1[i];
         delete y2[i];
-      }
+      }*/
     }
     void pot(void);
     void dep(void);
+
+    class Response : public NF
+    {
+      vector<double>& phi;
+      double lambda, mu;
+      double lminusm, expl, expm, factorlm;
+      double adjustedphi, deltaPdeltat, C1, dpdt, C1expl, C2expm, C1dtplusC2;
+      vector<double> value;
+      vector<double> dvdt;
+      vector<double> oldphi;
+    public:
+      void step(void);
+      void init( Configf& configf );
+      void restart( Restartf& restartf );
+      void dump( Dumpf& dumpf ) const;
+      double operator [] ( int index ) const;
+      Response( int nodes, double deltat, vector<double>& phi )
+          : NF(nodes,deltat,0),
+            phi(phi), value(nodes), dvdt(nodes), oldphi(nodes) {
+      }
+      ~Response(void) {}
+    };
+    Response xresponse; Response yresponse;
   };
 
   /*double alpha;
