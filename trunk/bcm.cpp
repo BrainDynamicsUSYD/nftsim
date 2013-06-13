af9ef7e9a752f1999e76b3c39613205655777549
ffung <ffung@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2013-06-13 03:11:55 +0000

    cTBS/iTBS predicted via BCM.

--- trunk/bcm.cpp
+++ trunk/bcm.cpp
@@ -1,5 +1,5 @@
-#include<deque>
-using std::deque;
+//#include<deque>
+//using std::deque;
 #include"bcm.h"
 
 void BCM::BCMDE::rhs( const vector<double>& y, vector<double>& dydt )
@@ -9,15 +9,21 @@ void BCM::BCMDE::rhs( const vector<double>& y, vector<double>& dydt )
   // recalculate dCadt with NMDAR plasticity
 
   // Ca
-  dydt[2] = y[8]*y[0]*y[1] -y[2]/tCa; // replace gnmda with y[6]
+  dydt[2] = y[8]*y[0]*y[1] -y[2]/tCa; // replace gnmda with y[8]
   if( y[2]+dydt[2]*deltat < 0 ) dydt[2] = -y[2];
+  // gNMDA
+  dydt[8] = -y[8]/t_BCM *(y[3]/y[7]-1) +(maxgnmda-y[8])/1000;
 }
 
-void BCM::init( Configf& configf )
+void BCM::BCMDE::init( Configf& configf )
 {
-  CaDP::init(configf);
+  CaDE::init(configf);
+  nu_init = variables[3][0];
+  configf.param("t_BCM",t_BCM);
   for( int i=0; i<nodes; i++ )
-    (*de)[8][i] = de->gnmda;
+    variables[8][i] = gnmda;
+  maxgnmda = gnmda;
+  //gnmda = t_BCM; // this hack is to allow BCM::init() to grab t_BCM
 }
 
 BCM::BCM( int nodes, double deltat, int index, const vector<double>& glu,
@@ -32,27 +38,34 @@ BCM::~BCM(void)
 {
 }
 
+/*void BCM::init( Configf& configf )
+{
+  CaDP::init(configf);
+  t_BCM = de->gnmda;
+  de->gnmda = (*de)[8][0];
+}*/
+
 void BCM::step(void)
 {
-  static deque<double> phihistory(int(5/deltat),8.87);
-  static double average = 8.87;
-  phihistory.push_back(prepropag[0]);
-  average += ( prepropag[0] -phihistory.front() )/(phihistory.size()-1);
-  phihistory.pop_front();
-  /*for( int i=0; i<phihistory.size(); i++ )
-    average += phihistory[i];
-  average /= phihistory.size();*/
+  /*static double init = 13e-6; //postpop()[0];
+  static deque<double> history( int(t_BCM/deltat),init );
+  static double average = init;
+  history.push_back( (*de)[3][0] ); //postpop()[0]
+  average += ( history.back() -history.front() )/(history.size()-1);
+  history.pop_front();
 
-  for( int i=0; i<nodes; i++ ) {
-    (*de)[8][i] = 8.87/average*2e-3;
-  }
+  for( int i=0; i<nodes; i++ )
+    (*de)[8][i] = 2e-3*pow( (*de)[7][0]/(*de)[3][0] ,2);*/
   CaDP::step();
 }
 
 void BCM::output( Output& output ) const
 {
   output.prefix("Couple",index+1);
-  output("nu",(*de)[3]);
+  output("nu",(*de)[7]);
+  output("nutilde",(*de)[3]);
   output("Ca",(*de)[2]);
   output("gNMDA",(*de)[8]);
+  output("x",(*de)[4]);
+  output("y",(*de)[5]);
 }
--- trunk/bcm.h
+++ trunk/bcm.h
@@ -8,19 +8,24 @@ class BCM : public CaDP
   BCM(BCM&);
   BCM();
 protected:
+  //double t_BCM;
   struct BCMDE : public CaDE
   {
+    double nu_init;
+    double t_BCM;
+    double maxgnmda;
     BCMDE( int nodes, double deltat ) : CaDE(nodes,deltat) {
       extend(1);
     }
     virtual ~BCMDE(void) {}
+    virtual void init( Configf& configf );
     virtual void rhs( const vector<double>& y, vector<double>& dydt );
   };
-  virtual void init( Configf& configf );
 public:
   BCM( int nodes, double deltat, int index, const vector<double>& glu,
           const Propag& prepropag, const Population& postpop );
   virtual ~BCM(void);
+  //virtual void init( Configf& configf );
   virtual void step(void);
   virtual void output( Output& output ) const;
 };
