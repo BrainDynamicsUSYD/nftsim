480a4c9af65f58d3fa6dcfb513467637db0f36c3
romesh <romesh@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2013-10-22 04:49:08 +0000

    Added NeuroField paper file

--- trunk/Helper_scripts/nf_eirs.m
+++ trunk/Helper_scripts/nf_eirs.m
@@ -239,7 +239,7 @@ function varargout = nf_eirs(p,file_id,firemode,int_time,grid_edge,fs,waves,rans
         fprintf(fid,'\n');
 
         fprintf(fid,'Output: Node: All Start: 0 Interval: 0.5e-2\n');
-        fprintf(fid,'Population: \n');
+        fprintf(fid,'Population: 1 3 4\n');
         fprintf(fid,'Dendrite:  \n');
         fprintf(fid,'Propag: 1\n');
         fprintf(fid,'Couple:  \n');
