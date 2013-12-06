564cef8bf34693700f91fac03cdb71fa6fc800ee
xzhao <xzhao@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2013-12-06 05:56:10 +0000

    updated dendriteramp

--- Helper_scripts/nf_spectrum.m
+++ Helper_scripts/nf_spectrum.m
@@ -7,17 +7,17 @@ function [f,P] = nf_spectrum( nf, data )
         data = nf_extract(nf,data);
     end
     
-    decimation_factor = (1/nf.deltat)/200; % Downsample to 200Hz
-    if decimation_factor ~= round(decimation_factor) % Unless it is not integer
+%     decimation_factor = (1/nf.deltat)/200; % Downsample to 200Hz
+%     if decimation_factor ~= round(decimation_factor) % Unless it is not integer
         decimation_factor = 1;
-    end
-    
+%     end
+%     
     v = decimate(data, decimation_factor );
     [f,~,P] = rfft(v.*hamming(length(v)), 1/nf.deltat/decimation_factor );
     
     figure
     loglog(f,P);
-    set(gca,'XLim',[1 45]);
+    set(gca,'XLim',[0.1 50]);
     xlabel('Frequency (Hz)');
     ylabel('P(f) Spectral Power Density (arbitrary normalization)');
     title(sprintf('Power spectrum (at %iHz sampling)',1/nf.deltat/decimation_factor));
