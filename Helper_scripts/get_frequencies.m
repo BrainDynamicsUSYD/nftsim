8267b9cc10d0e9d70d1b20b9f546952abc52903d
romesh <romesh@a4b902c6-8344-0410-a935-8ddf1f1c9dd8>
2014-11-14 03:10:47 +0000

    Output df,dk,dx and added compatibility mode for nf_read for very large files

--- Helper_scripts/get_frequencies.m
+++ Helper_scripts/get_frequencies.m
@@ -1,4 +1,4 @@
-function [f,Kx,Ky,x,y] = get_frequencies(data,fs,Lx,Ly)
+function [f,Kx,Ky,x,y,df,dk,dx] = get_frequencies(data,fs,Lx,Ly)
 	% Take in the size of the data matrix, the sampling rate, and spatial distances
 	% Return corresponding grids of frequencies and positions
 	Kx = [];
@@ -12,6 +12,8 @@ function [f,Kx,Ky,x,y] = get_frequencies(data,fs,Lx,Ly)
     end
 
     f = (0:fs/size(data,3):fs/2).'; % This is the single sided frequency
+    df = fs/size(data,3);
+    
     Kx = 2*pi*(0:1/Lx:size(data,1)/Lx/2); % This is the single sided frequency
     Ky = 2*pi*(0:1/Ly:size(data,2)/Ly/2); % This is the single sided frequency
     
@@ -27,6 +29,7 @@ function [f,Kx,Ky,x,y] = get_frequencies(data,fs,Lx,Ly)
         Ky = [-Ky(end:-1:2) Ky(1:end-1)];
     end
     [Kx,Ky] = meshgrid(Kx,Ky);
+    dk = 2*pi/Lx;
 
     dx = Lx/size(data,1); % Metres per pixel
     x = dx*(0:size(data,1)-1);
