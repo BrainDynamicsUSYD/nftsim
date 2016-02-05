1f6105683a8c814cd3d4cef045d1c17fdee54a50
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2016-02-05 12:30:35 +0000

    Updated spectrum to be comparable to spatial_spectrum

--- +nf/spectrum.m
+++ +nf/spectrum.m
@@ -1,25 +1,33 @@
-function [f,P] = spectrum( obj, data )
-    % return the frequency and frequency spectrum of given data
-    % nf is parsed for information on sampling time
-    % data can be a string specifying a trace, or a vector
-    
-    if strcmp(class(data),'char')
-        data = obj.extract(obj,data);
+function [f,P] = spectrum(obj,p,n_windows)    % return the frequency and frequency spectrum of given data
+    %  nf - nf object
+    %  p - trace to use  
+
+    %  nf -> a 3D matrix of values
+    %  p - sampling rate
+    if isstruct(obj)
+        if nargin < 2 || isempty(p)
+            p = 'propag.1.phi'; % Try the phi propagator first
+        end
+        data = nf.grid(obj,p);
+        fs = 1/obj.deltat;
+    else
+        data = obj;
+        fs = p;
     end
+
+    if nargin < 3 || isempty(n_windows)
+        n_windows = 8; % 8 windows by default
+    end
+
+    frac_overlap = 0.5;
+    window_idx = nf.partition(size(data,2),n_windows,[],frac_overlap,1,1);
     
-%     decimation_factor = (1/nf.deltat)/200; % Downsample to 200Hz
-%     if decimation_factor ~= round(decimation_factor) % Unless it is not integer
-        decimation_factor = 1;
-%     end
-%     
-    v = decimate(data, decimation_factor );
-    [P,f] = pwelch(v, [],[],[],1/obj.deltat/decimation_factor );
-    
-    figure
-    loglog(f,P);
-    set(gca,'XLim',[0.1 50]);
-    xlabel('Frequency (Hz)');
-    ylabel('P(f) Spectral Power Density (arbitrary normalization)');
-    title(sprintf('Power spectrum (at %iHz sampling)',1/obj.deltat/decimation_factor));
+    for j = 1:length(window_idx)
+    [f,~,P1(:,j)] = utils.rfft(data(window_idx(j,1):window_idx(j,2)),200);
+    end
+
+    P = mean(P1,2);
+
+
     
 end
