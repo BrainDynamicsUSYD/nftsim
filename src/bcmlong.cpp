cfd6b6fd2a73d0ea7a317108828f1329706e8c44
Stuart Knock <stuart.knock@gmail.com>
2016-09-22 22:30:00 +1000

    Conform to C++11 standard (clang-tidy). Standardises formatting (astyle).

--- src/bcmlong.cpp
+++ src/bcmlong.cpp
@@ -1,13 +1,13 @@
 #include<cstring>
 #include"bcmlong.h"
 
-void BCMLong::init( Configf& configf )
-{
+void BCMLong::init( Configf& configf ) {
   int position1 = configf.tell();
   // first obtain parameters
   de_2d[0]->init(configf);
-  for( int i=0; i<nodes; i++ )
+  for( int i=0; i<nodes; i++ ) {
     *de_2d[i] = *de_2d[0];
+  }
 
   // then re-read nu-matrix
   int position2 = configf.tell();
@@ -17,20 +17,23 @@ void BCMLong::init( Configf& configf )
   vector<double> temp = configf.numbers();
   if( temp.size() == 1 ) {
     // diagonals == temp[0], off-diagonals ~= 0
-    for( int i=0; i<nodes; i++ )
-      for( int j=0; j<nodes; j++ )
+    for( int i=0; i<nodes; i++ ) {
+      for( int j=0; j<nodes; j++ ) {
         (*de_2d[i])[7][j] = (*de_2d[i])[3][j] = 1e-8;
-    for( int i=0; i<nodes; i++ )
+      }
+    }
+    for( int i=0; i<nodes; i++ ) {
       (*de_2d[i])[7][i] = (*de_2d[i])[3][i] = temp[0];
+    }
     pos = (temp[0]>0)?1:-1;
-  }
-  else if( temp.size() == size_t(pow(nodes,2)) ) {
-    for( int i=0; i<nodes; i++ )
-      for( int j=0; j<nodes; j++ )
+  } else if( temp.size() == size_t(pow(nodes,2)) ) {
+    for( int i=0; i<nodes; i++ ) {
+      for( int j=0; j<nodes; j++ ) {
         (*de_2d[i])[7][j] = (*de_2d[i])[3][j] = temp[i+j*nodes];
+      }
+    }
     pos = (temp[0]>0)?1:-1;
-  }
-  else {
+  } else {
     cerr<<"Matrix of nu either has a homogeneous initial value or takes in node^2 values."<<endl;
     exit(EXIT_FAILURE);
   }
@@ -39,31 +42,29 @@ void BCMLong::init( Configf& configf )
 
   for( int j=0; j<nodes; j++ ) {
     P[j] = 0;
-    for( int i=0; i<nodes; i++ )
+    for( int i=0; i<nodes; i++ ) {
       P[j] += (*de_2d[i])[7][j]*prepropag[i];
+    }
   }
 }
 
 BCMLong::BCMLong( int nodes, double deltat, int index,
-      const Propag& prepropag, const Population& postpop )
-    : BCM(nodes,deltat,index,prepropag,postpop), de_2d(nodes), rk4_2d(nodes)
-{
+                  const Propag& prepropag, const Population& postpop )
+  : BCM(nodes,deltat,index,prepropag,postpop), de_2d(nodes), rk4_2d(nodes) {
   for( int i=0; i<nodes; i++ ) {
     de_2d [i] = new BCMDE(nodes,deltat);
     rk4_2d[i] = new RK4(*de_2d[i]);
   }
 }
 
-BCMLong::~BCMLong(void)
-{
+BCMLong::~BCMLong() {
   for( int i=0; i<nodes; i++ ) {
     delete de_2d[i];
     delete rk4_2d[i];
   }
 }
 
-void BCMLong::step(void)
-{
+void BCMLong::step() {
   // (*de_2d[i])[k][j] : i=from node i, j=to node j, k=kth field of y
   // y == { binding, H, Ca, nutilde, x, y, dnudt, nu, nu_Ca }
 
@@ -71,11 +72,12 @@ void BCMLong::step(void)
   // Ca   [i][j] -> nu_Ca[i][j], B[i], H[j], Ca[i][j]
   // nu_Ca[i][j] -> nu_Ca[i][j], nutilde[i][j], nu[i][j]
 
-  for( int i=0; i<nodes; i++ )
+  for( int i=0; i<nodes; i++ ) {
     for( int j=0; j<nodes; j++ ) {
       (*de_2d[i])[0][j] = de_2d[0]->sig( postpop.glu()[i] -de_2d[0]->glu_0, de_2d[0]->B );
       (*de_2d[i])[1][j] = (195e-3-postpop.V()[j])*de_2d[0]->sig( postpop.V()[j]-45.5e-3,62 );
     }
+  }
 
   for( int i=0; i<nodes; i++ ) {
     de_2d[i]->pot();
@@ -85,13 +87,13 @@ void BCMLong::step(void)
 
   for( int j=0; j<nodes; j++ ) {
     P[j] = 0;
-    for( int i=0; i<nodes; i++ )
+    for( int i=0; i<nodes; i++ ) {
       P[j] += (*de_2d[i])[7][j]*prepropag[i];
+    }
   }
 }
 
-void BCMLong::output( Output& output ) const
-{
+void BCMLong::output( Output& output ) const {
   // outputting a 2D matrix rather than a vector:
   // for each "to", output all "from"
   for( int i=0; i<nodes; i++ ) {
