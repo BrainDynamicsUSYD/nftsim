767a4317dfccba92deb6836ec005a329552e5722
Stuart Knock <stuart.knock@gmail.com>
2016-09-22 22:29:02 +1000

    Conform to C++11 standard (clang-tidy). Standardises formatting (astyle).

--- src/configf.cpp
+++ src/configf.cpp
@@ -8,8 +8,7 @@ using std::cerr;
 using std::string;
 
 Configf::Configf( const char* filename )
-  : std::ifstream(filename)
-{
+  : std::ifstream(filename) {
   if( !*this ) {
     cerr << "Unable to open configuration file." << endl;
     exit(EXIT_FAILURE);
@@ -22,27 +21,26 @@ Configf::Configf( const char* filename )
   seekg(sp);
 }
 
-Configf::~Configf()
-{
+Configf::~Configf() {
   delete[] buffer;
 }
 
 // Reads an arbitrary number of doubles and return it in an array
-vector<double> Configf::numbers(void)
-{
+vector<double> Configf::numbers() {
   vector<double> ret;
   while( good() ) { // read until non-numeral
-    double buffer; *this >> buffer;
-	ret.push_back( buffer );
+    double buffer;
+    *this >> buffer;
+    ret.push_back( buffer );
   }
   ret.pop_back(); // deletes last element==0
   clear(); // clear the fail bit
   return ret;
 }
 
-vector<string> Configf::arb( const string& delim )
-{
-  string temp; vector<string> ret_value;
+vector<string> Configf::arb( const string& delim ) {
+  string temp;
+  vector<string> ret_value;
   while( temp!=delim && *this ) {
     *this >> temp;
     ret_value.push_back(temp);
@@ -53,28 +51,28 @@ vector<string> Configf::arb( const string& delim )
     }
   }
   ret_value.pop_back();
-  for( size_t i=0; i<delim.size(); i++ )
+  for( size_t i=0; i<delim.size(); i++ ) {
     unget();
+  }
   return ret_value;
 }
 
-string Configf::find( const string& Check )
-{
-  /* 
+string Configf::find( const string& Check ) {
+  /*
   This function implements a wildcard search that searches the config file
   assuming a structure like
     <unique>: extra_data - key1:value key2:value
   The search term Check takes the form "<unique>*keyn" or "<unique>"
-  and this function returns a string representation of the value associated 
+  and this function returns a string representation of the value associated
   with the key that was being searched for. If the search term is "<unique>"
-  then a string representation of extra_data is returned. The file pointer is 
+  then a string representation of extra_data is returned. The file pointer is
   returned to the same position as it was originally.
   Search is CASE SENSITIVE
   */
 
   if( Check.empty() ) {
     cerr << "Attempted to use Configf::Find searching for an empty string"
-            << endl;
+         << endl;
     exit(EXIT_FAILURE);
   }
   string result;
@@ -88,34 +86,33 @@ string Configf::find( const string& Check )
     cerr << "Failed to find '" << Check.substr(0,wildcard_pos) << "' which was the first part of the search term '" << Check << "'" << endl;
     exit(EXIT_FAILURE);
   }
-  
+
   // skip searching for the second part if a star is not present
-  if(wildcard_pos != string::npos){
-      match = file_content.find(Check.substr(wildcard_pos+1,Check.length()),match);
-      if( match == string::npos) {
-        seekg(sp);
-        return string("");
-    /*cerr << "Failed to find '" << Check.substr(wildcard_pos+1,Check.length()) << "' which was the second part of the search term '" << Check << "'" << endl;
-        exit(EXIT_FAILURE);*/
-      }
+  if(wildcard_pos != string::npos) {
+    match = file_content.find(Check.substr(wildcard_pos+1,Check.length()),match);
+    if( match == string::npos) {
+      seekg(sp);
+      return string("");
+      /*cerr << "Failed to find '" << Check.substr(wildcard_pos+1,Check.length()) << "' which was the second part of the search term '" << Check << "'" << endl;
+          exit(EXIT_FAILURE);*/
+    }
     match += Check.substr(wildcard_pos+1,Check.length()).length();
 
-  }
-  else{
+  } else {
     match += Check.length();
   }
-  
+
   // adjust the position if the user didn't include a colon in the search term
-  if(Check[Check.length()-1] != ':')
+  if(Check[Check.length()-1] != ':') {
     match += 1;
+  }
   seekg(0+match);
   *this >> result;
   seekg(sp);
   return string(result);
 }
 
-bool Configf::next( const string& Check, int delim )
-{
+bool Configf::next( const string& Check, int delim ) {
   if( Check.empty() ) {
     cerr << "Configf validating an empty string." << endl;
     exit(EXIT_FAILURE);
@@ -135,8 +132,7 @@ bool Configf::next( const string& Check, int delim )
   return true;
 }
 
-void Configf::go2( const string& keyword )
-{
+void Configf::go2( const string& keyword ) {
   seekg(0,std::ios::beg);
   if( keyword.empty() ) {
     cerr << "Searching for an empty string in configuration file." << endl;
@@ -152,8 +148,7 @@ void Configf::go2( const string& keyword )
   seekg( file_content.find(keyword)-1 );
 }
 
-string label( const string& prefix, int index )
-{
+string label( const string& prefix, int index ) {
   stringstream ss;
   ss<<prefix.c_str()<<index;
   return ss.str();
