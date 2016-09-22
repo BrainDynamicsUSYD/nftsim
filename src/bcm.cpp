236a4ddacb9b6144af5a63916a58a76c801b1ce6
Stuart Knock <stuart.knock@gmail.com>
2016-09-22 22:30:16 +1000

    Conform to C++11 standard (clang-tidy). Standardises formatting (astyle).

--- src/bcm.cpp
+++ src/bcm.cpp
@@ -1,45 +1,47 @@
 #include<cmath>
 #include"bcm.h"
 
-void BCM::BCMDE::rhs( const vector<double>& y, vector<double>& dydt )
-{
+void BCM::BCMDE::rhs( const vector<double>& y, vector<double>& dydt ) {
   // y == { binding, H, Ca, nutilde, x, y, dnudt, nu, gNMDA }
   CaDE::rhs(y,dydt);
   // recalculate dCadt with NMDAR plasticity
 
   // Ca
   dydt[2] = y[8]*y[0]*y[1] -y[2]/tCa; // replace gnmda with y[8]
-  if( y[2]+dydt[2]*deltat < 0 ) dydt[2] = -y[2];
+  if( y[2]+dydt[2]*deltat < 0 ) {
+    dydt[2] = -y[2];
+  }
   // gNMDA
   dydt[8] = -y[8]/t_BCM *(y[3]/y[7]-1) +(gnmda_0-y[8])/t_rec;
-  if( y[7]==0 ) dydt[8] = 0;
+  if( y[7]==0 ) {
+    dydt[8] = 0;
+  }
 }
 
-void BCM::BCMDE::init( Configf& configf )
-{
+void BCM::BCMDE::init( Configf& configf ) {
   CaDE::init(configf);
   configf.param("t_BCM",t_BCM);
-  for( int i=0; i<nodes; i++ )
+  for( int i=0; i<nodes; i++ ) {
     variables[8][i] = gnmda;
+  }
   gnmda_0 = gnmda;
-  if( !configf.optional("t_rec",t_rec) )
+  if( !configf.optional("t_rec",t_rec) ) {
     t_rec = 1e3;
+  }
 }
 
 BCM::BCM( int nodes, double deltat, int index,
-        const Propag& prepropag, const Population& postpop )
-    : CaDP(nodes,deltat,index,prepropag,postpop)
-{
-  delete de; de = new BCMDE(nodes,deltat);
-  delete rk4; rk4 = new RK4(*de);
+          const Propag& prepropag, const Population& postpop )
+  : CaDP(nodes,deltat,index,prepropag,postpop) {
+  delete de;
+  de = new BCMDE(nodes,deltat);
+  delete rk4;
+  rk4 = new RK4(*de);
 }
 
-BCM::~BCM(void)
-{
-}
+BCM::~BCM() = default;
 
-void BCM::output( Output& output ) const
-{
+void BCM::output( Output& output ) const {
   output.prefix("Couple",index+1);
   output("nu",(*de)[7]);
   output("nutilde",(*de)[3]);
