a9a399129f5843bd5ed99dba7743f450eb2334f9
Stuart Knock <stuart.knock@gmail.com>
2016-09-22 22:28:47 +1000

    Conform to C++11 standard (clang-tidy). Standardises formatting (astyle).

--- src/couple.cpp
+++ src/couple.cpp
@@ -3,61 +3,55 @@ using std::cerr;
 using std::endl;
 #include "couple.h"
 
-void Couple::init( Configf& configf )
-{
+void Couple::init( Configf& configf ) {
   configf.next("nu");
   vector<double> temp = configf.numbers();
   if( temp.size() == 1 ) {
-    n.clear(); n.resize(nodes,temp[0]);
+    n.clear();
+    n.resize(nodes,temp[0]);
     pos = (temp[0]>0)?1:-1;
-  }
-  else if( temp.size() == size_t(nodes) ) {
-    n.clear(); n.resize(nodes);
-    for( int i=0; i<nodes; i++ )
+  } else if( temp.size() == size_t(nodes) ) {
+    n.clear();
+    n.resize(nodes);
+    for( int i=0; i<nodes; i++ ) {
       n[i] = temp[i];
+    }
     pos = 0;
-  }
-  else {
+  } else {
     cerr<<"nu either has a homogeneous initial value or has one intial value per node."<<endl;
     exit(EXIT_FAILURE);
   }
-  for( int i=0; i<nodes; i++ )
+  for( int i=0; i<nodes; i++ ) {
     P[i] = n[i]*prepropag.phiinit(configf);
+  }
 }
 
 Couple::Couple( int nodes, double deltat, int index,
-        const Propag& prepropag, const Population& postpop )
-    : NF(nodes,deltat,index), prepropag(prepropag), postpop(postpop), n(nodes), P(nodes)
-{
+                const Propag& prepropag, const Population& postpop )
+  : NF(nodes,deltat,index), prepropag(prepropag), postpop(postpop), n(nodes), P(nodes) {
 }
 
-Couple::~Couple(void)
-{
-}
+Couple::~Couple() = default;
 
-void Couple::step(void)
-{
-  for( int i=0; i<nodes; i++ )
+void Couple::step() {
+  for( int i=0; i<nodes; i++ ) {
     P[i] = n[i]*prepropag[i];
+  }
 }
 
-double Couple::nuinit( Configf& configf ) const
-{
+double Couple::nuinit( Configf& configf ) const {
   string buffer = configf.find( label("Couple ",index+1)+"*nu:");
   return atof(buffer.c_str());
 }
 
-void Couple::output( Output& output ) const
-{
+void Couple::output( Output& output ) const {
   output("Couple",index+1,"nu",n);
 }
 
-bool Couple::excite(void) const
-{
+bool Couple::excite() const {
   return pos==1;
 }
 
-const vector<double>& Couple::nuphi(void) const
-{
+const vector<double>& Couple::nuphi() const {
   return P;
 }
