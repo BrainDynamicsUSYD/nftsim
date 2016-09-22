63bd41dbbdb2845463d881d42a344dc72e409e6d
Stuart Knock <stuart.knock@gmail.com>
2016-09-22 22:22:50 +1000

    Conform to C++11 standard (clang-tidy). Standardises formatting (astyle).

--- src/random.cpp
+++ src/random.cpp
@@ -2,30 +2,28 @@
 
 int Random::id = 0;
 
-Random::Random(double mean = 0.0, double std = 1.0){
+Random::Random(double mean, double std) {
   init(++id,mean,std);
 }
 
-Random::Random(int seed,double mean = 0.0, double std = 1.0){
+Random::Random(int seed, double mean, double std) {
   init(seed,mean,std);
   ++id;
 }
 
-void Random::init(int seed,double mean, double std){
+void Random::init(int seed, double mean, double std) {
   gen = std::mt19937_64(seed); // Generator
   dist = std::normal_distribution<double>(mean,std); // Distribution
 }
 
-void Random::get(double& target){
+void Random::get(double& target) {
   target = dist(gen);
 }
 
-double Random::get(void){
+double Random::get() {
   double temp;
   get(temp);
   return temp;
 }
 
-Random::~Random()
-{
-}
+Random::~Random() = default;
