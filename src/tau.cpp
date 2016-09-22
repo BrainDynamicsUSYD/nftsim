90d3d9513a66d6055e42381d5943d1c5208b5fec
Stuart Knock <stuart.knock@gmail.com>
2016-09-22 22:20:11 +1000

    Conform to C++11 standard (clang-tidy). Standardises formatting (astyle).

--- src/tau.cpp
+++ src/tau.cpp
@@ -3,8 +3,7 @@
 using std::cerr;
 using std::endl;
 
-void Tau::init( Configf& configf )
-{
+void Tau::init( Configf& configf ) {
   vector<double> temp = configf.numbers();
   if( temp.size() == 1 ) {
     if( remainder(temp[0],deltat) >deltat ) {
@@ -13,8 +12,7 @@ void Tau::init( Configf& configf )
     }
     m[0] = temp[0]/deltat;
     max = m[0];
-  }
-  else if( temp.size() == size_t(nodes) ) {
+  } else if( temp.size() == size_t(nodes) ) {
     if( remainder(temp[0],deltat) >deltat ) {
       std::cerr<<"Value of tau not divisible by Deltat!"<<endl;
       exit(EXIT_FAILURE);
@@ -22,24 +20,20 @@ void Tau::init( Configf& configf )
     m.resize(nodes);
     for( int i=0; i<nodes; i++ ) {
       m[i] = int(temp[i]/deltat);
-      if( m[i]>max )
+      if( m[i]>max ) {
         max = m[i];
+      }
     }
-  }
-  else {
+  } else {
     std::cerr << "The number of Tau has to be one or the same as the number of nodes." << endl;
     exit(EXIT_FAILURE);
   }
 }
 
-Tau::Tau( int nodes, double deltat, int index ) : NF(nodes,deltat,index), max(0), m(1,0)
-{
+Tau::Tau( int nodes, double deltat, int index ) : NF(nodes,deltat,index), max(0), m(1,0) {
 }
 
-Tau::~Tau()
-{
-}
+Tau::~Tau() = default;
 
-void Tau::step(void)
-{
+void Tau::step() {
 }
