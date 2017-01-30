8dcddfffbaaebcd29aeb44416ad7603c335d0214
Stuart Knock <stuart.knock@gmail.com>
2017-01-30 16:28:05 +1100

    Adds dendrite_integral.h and dendrite_integral.cpp
      This is in preparation for making a differential form of
      the Dendrite::step() member-function the default. The two
      new files introduce a DendriteIntegral class that derives
      from Dendrite and is intended to preserve the previous
      default integral form for future use.

---
+++ src/dendrite_integral.h
@@ -0,0 +1,42 @@
+/** @file dendrite_integral.h
+  @brief Defines the DendriteIntegral class, which handles the response of the
+         postsynaptic population using the integral form for the step()
+         member-function.
+
+  A more detailed multiline description...
+
+  @author Peter Drysdale, Felix Fung,
+*/
+
+#ifndef NEUROFIELD_SRC_DENDRITE_INTEGRAL_H
+#define NEUROFIELD_SRC_DENDRITE_INTEGRAL_H
+
+#include "dendrite.h"
+
+class DendriteIntegral : public Dendrite {
+  DendriteIntegral(void); // default constructor
+  DendriteIntegral(DendriteIntegral& ); // no copy constructor
+
+  // variables that are intialized once to speed up computation
+  double aminusb;  ///< == alpha - beta
+  double expa;     ///< == exp(-alpha*deltat)
+  double expb;     ///< == exp(-beta*deltat)
+  double factorab; ///< == 1./alpha + 1./beta;
+
+  // variables that are used every timestep
+  double adjustednp;
+  double deltaPdeltat; //NOTE: doesn't seem to be used anywhere (???maybe replaced by current dpdt???).
+  double C1;
+  double dpdt; // Temporal derivative of pulse density.
+  double C1expa;
+  double C2expb;
+  double C1dtplusC2;
+ public:
+
+  DendriteIntegral( size_type nodes, double deltat, size_type index,
+            const Propagator& prepropag, const Coupling& precouple );
+  ~DendriteIntegral(void) override;
+  void step(void) override;
+};
+
+#endif //NEUROFIELD_SRC_DENDRITE_INTEGRAL_H
