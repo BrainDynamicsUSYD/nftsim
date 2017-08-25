be9d7c869155445c9f4c1d7a2cb1ca9d67d055bc
Paula Sanz-Leon <paula.sanz-leon@sydney.edu.au>
2017-08-25 17:26:31 +1000

    Generalizes to reshape rectangular grids

--- +nf/grid.m
+++ +nf/grid.m
@@ -1,4 +1,5 @@
-%% This function returns a gridded version of a single parameter.
+%% This function reshapes a 2D array into a 3D array, with the 1st and 2nd dimensions, 
+%  corresponding to X and Y coordinates and the 3rd dimension corresponding to time. 
 %
 % e.g. data = nf.grid(nf,'Propagator.2.phi') will return the data for Propagator.2.phi
 % as a 3D matrix, with X and Y coordinates referring to grid positions and
@@ -7,12 +8,12 @@
 % output all nodes), or the number of nodes is not a perfect square..
 %
 % ARGUMENTS:
-%        obj -- A neurofield output struct (a Matlab struct containing data
-%               from a simulation).
-%        trace -- .
+%        obj   -- A neurofield output struct (a Matlab struct containing data
+%                 from a simulation).
+%        trace -- A string with the name of the array to reshape. 
 %
 % OUTPUT:
-%        data -- .
+%        data  -- The reshaped array specified in trace.
 %        grid_edge -- .
 %
 % REQUIRES:
@@ -23,26 +24,33 @@
 %
 % USAGE:
 %{
-    %
+
     
 %}
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [data, grid_edge] = grid(obj, trace)
+function [data, longside_nodes, shortside_nodes] = grid(obj, trace)
     %
-    nodes = obj.nodes{1};
-    if nodes(1) ~= 1 % || any(diff(nodes))~=1
+    output_nodes = length(obj.nodes{1});
+    
+    if output_nodes ~= obj.input_nodes 
         error(['nf:' mfilename ':IncompatibleOutput'], ...
               'Output from NeuroField must be for all nodes')
     end
     
     data = nf.extract(obj, trace);
     %TODO: handle non-square grid here.
-    grid_edge = sqrt(length(nodes)); % length of square grid edge
     
-    %Reshape to a square grid by time, ie (n,n,tpts)
+    if ~isfield(obj, 'longside_nodes')
+        longside_nodes  = sqrt(length(output_nodes)); % length of square grid edge
+        shortside_nodes = longside_nodes;
+    else
+        longside_nodes = obj.longside_nodes;
+        shortside_nodes = obj.input_nodes / obj.longside_nodes;
+    end
+    %Reshape to an array of (n,m,tpts)
 
     %data = reshape(data, grid_edge, grid_edge, obj.npoints);
     
-    data = shiftdim(reshape(data, [], grid_edge, grid_edge), 1);
+    data = shiftdim(reshape(data, [], longside_nodes, shortside_nodes), 1);
 end %function grid()
