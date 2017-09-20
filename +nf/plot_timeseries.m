e632740e364d99eb2f25f66c5cf3e5145476d047
Paula Sanz-Leon <paula.sanz-leon@sydney.edu.au>
2017-09-20 16:59:31 +1000

    Replaces tabs with spaces and improves indentation

--- +nf/plot_timeseries.m
+++ +nf/plot_timeseries.m
@@ -28,39 +28,41 @@
     nf.plot_timeseries(nf_obj, {'Propagator.1.phi'}, {[8080:8120]})
 %}
 
-
-
-
 function figure_handles =  plot_timeseries(obj, traces, node_idx, scaling)
 
+  % Get some size info about data
   num_figs   = length(traces);
   time       = obj.time;
   time_steps  = length(time);
   separate_by = zeros(1, num_figs);
 
 
-  for nof=1:num_figs,
+  for nof=1:num_figs
     num_nodes = length(node_idx{nof});
-    for k = 1:num_nodes, 
+    for k = 1:num_nodes
         labels{k} = num2str(node_idx{nof}(k)); 
-    end; %index labels
+    end %index labels
 
-    figure_handles{nof} = figure(nof),
-    data     = nf.extract(obj, traces{nof});
-    data     = detrend(data);
+    figure_handles{nof} = figure(nof);
+    fig_title = [traces{nof}];
+    % Get fata to plot
+    data = nf.extract(obj, traces{nof});
+    data = detrend(data);
 
-    % scale data to the range [0, 1]
+    % Rescale data to the range [0, 1]
     data = data - min(data(:)); 
     data = data ./ max(data(:));
 
-    if scaling
-    	datamean = mean(data, 2);
+    if scaling % this scaling is similar to the case 1 in nf.movie, it removes the spatial average at each time frame
+        datamean = mean(data, 2);
         data = bsxfun(@minus, data, datamean);
     end
+
     separate_by(1,nof) = 0.33*(max(data(:)) - min(data(:)))
     nodes_by_timestepsize = repmat((1:num_nodes), [time_steps,1]);
-    plot(time, data(:, node_idx{nof}) + separate_by(1,nof)*nodes_by_timestepsize, 'color', [0.42, 0.42, 0.42]);
-    fig_title = [traces{nof}];
+    
+    plot(time, data(:, node_idx{nof}) + separate_by(1,nof)*nodes_by_timestepsize, 'color', [0.42, 0.42, 0.42]); 
+    
     title(fig_title, 'interpreter', 'none');
     xlabel('Time [s]');
     ylabel('Node index')
