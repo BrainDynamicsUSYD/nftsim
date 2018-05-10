1dcf7ba338add885a393dd0261631e1db8265629
Paula Sanz-Leon <p.sanz-leon@physics.usyd.edu.au>
2018-05-10 14:42:31 +1000

    Shift Fourier components to have the centre at 0.

--- +nf/rfft.m
+++ +nf/rfft.m
@@ -8,12 +8,12 @@
 %        y  -- a 2D array of size (timepoints x N), where N is the number of signals/timeseries or traces.
 %        fs -- sampling frequency (Hz) of the time series.
 %        NFFT -- override the amount of zero padding in the FFT.
-%        windowed -- set as true to apply a Hamming window.
+%        windowed  -- set as true to apply a Hamming window.
 %        detrended -- set as true to remove any constant offset and linear trend.
 %
 % OUTPUT:
 %        f -- frequency values
-%        s -- Fourier components, denoted c_k in Chris Rennie's notes.
+%        Y -- Fourier components, denoted c_k in Chris Rennie's notes.
 %        P -- (fpts, N) Mean spectral power density, correct power is obtained by
 %             integrating w.r.t. frequency values (f).
 %
@@ -27,11 +27,11 @@
 % USAGE:
 %{
     %
-    [f, s, P] = nf.rfft(y, fs, NFFT)
+    [f, Y, P] = nf.rfft(y, fs, NFFT)
 %}
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [f, Y, P, varargout] = rfft(y, fs, NFFT, windowed, detrended, one_sided)
+function [f, Y, P] = rfft(y, fs, NFFT, windowed, detrended, one_sided)
     %
     if nargin < 6
        one_sided = true;
@@ -89,6 +89,7 @@ function [f, Y, P, varargout] = rfft(y, fs, NFFT, windowed, detrended, one_sided
         end
             
     else
+        Y = fftshift(Y);
         f = -(fs/2)+freq_bin_width:freq_bin_width:(fs/2);
         P = abs(Y).^2; % two sided spectrum 
 
